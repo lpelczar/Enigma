e798ac37e4ade4382057ca05fb897426e40e27b9
Piotr Górski <piotrek.gorski90@gmail.com>
2017-12-14 18:12:02 +0100

    Refactor KeywordEnigma

--- src/enigma/KeywordEnigma.java
+++ src/enigma/KeywordEnigma.java
@@ -6,69 +6,67 @@ import services.EnigmaService;
 
 public class KeywordEnigma implements EnigmaService {
 
-    public static final boolean KEY_REQUIRED = true; //publiczna statyczna zmienna ktorej wartosc sie nie zmieni
-    private String name;        //deklaruje zmienna name
+    public static final boolean KEY_REQUIRED = true;
+    private static final String NAME = "KeywordEnigma";
+    private String name;
     private String key;
 
-    public KeywordEnigma(String name){
-        this.name = name;
-        this.key = key;
-    }
 
-    public char[] encipher_alphabet(String key){
-		char[] keyword_alphabet = new char[26];
+    public char[] encipherAlphabet(String key){
+        int alphabetLength = 26;
+		char[] keywordAlphabet = new char[alphabetLength];
 		for (int i = 0; i < key.length(); i++) {
-		    keyword_alphabet[i] = key.charAt(i);
+		    keywordAlphabet[i] = key.charAt(i);
 		}
-		char c = 'A' - 1; // zmiana upper lower jak potrzeba
-		for (int i = key.length(); i < 26; i++) { //wchodzimy na indeks o dlugosci slowa +1 i dajemy tam
-		                                                                          //char c +1 czyli kolejne litery alfabetu
-		    while (key.contains(String.valueOf(++c)));   // loop dopoki nie znajdziemy char w word
-		keyword_alphabet[i] = c; //indent czy nie
+		char c = 'A' - 1;
+		for (int i = key.length(); i < alphabetLength; i++) {
+		    while (key.contains(String.valueOf(++c)));
+		keywordAlphabet[i] = c;
 		}
-        return keyword_alphabet;
+        return keywordAlphabet;
 	}
 
     public String encipher(String text){
-        String enc_text = "";
+        int asciiValue = 65;
         String message = text.toUpperCase().replaceAll("[^a-zA-Z ]", "");
-        char[] enc_alphabet = encipher_alphabet(this.key);
+        String encText = "";
+        char[] encAlphabet = encipherAlphabet(this.key);
         for (int i = 0; i < message.length(); i++){
             char letter = message.charAt(i);
             if (letter >='A' && letter <='Z'){
-                int pos = letter - 65;
-                enc_text += enc_alphabet[pos];
+                int pos = letter - asciiValue;
+                encText += encAlphabet[pos];
             }
             else
             {
-                enc_text += letter;
+                encText += letter;
             }
         }
-        return enc_text;
+        return encText;
     }
 
 	public String decipher(String text){
 
         String message = text.toUpperCase().replaceAll("[^a-zA-Z ]", "");
-        char[] enc_alphabet = encipher_alphabet(this.key);
-        String dec_text = "";
+        char[] encAlphabet = encipherAlphabet(this.key);
+        String decText = "";
         char [] plainAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        String enc_text = encipher(message);
+        String encText = encipher(message);
 
         for (int i = 0; i < message.length(); i++){
-            char enc_let = enc_text.charAt(i);
+            char encLetter = encText.charAt(i);
             char letter = message.charAt(i);
             if (letter >='A' && letter <='Z'){
-                int pos = letter - enc_let;
-                dec_text += plainAlphabet[pos];
+                int pos = letter - encLetter;
+                decText += plainAlphabet[pos];
             }
             else
             {
-                dec_text += letter;
+                decText += letter;
 
             }
         }
-        return dec_text;
+        return decText;
 }
 
     public String getName(){
