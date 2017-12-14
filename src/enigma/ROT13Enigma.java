7bfb98e0b208f193fa2f7012a9589b5a3bafec4f
Piotr Górski <piotrek.gorski90@gmail.com>
2017-12-14 18:11:34 +0100

    Full ROT13 refactor

--- src/enigma/ROT13Enigma.java
+++ src/enigma/ROT13Enigma.java
@@ -4,21 +4,18 @@ import services.EnigmaService;
 
 public class ROT13Enigma implements EnigmaService {
 
-    public static final boolean KEY_REQUIRED = false; //publiczna statyczna zmienna ktorej wartosc sie nie zmieni
-    private String name;        //deklaruje zmienna name
-
-    public ROT13Enigma(String name){
-        this.name = name;
-    }
+    public static final boolean KEY_REQUIRED = false;
+    private static final String NAME = "ROT13Enigma";
+    private String name;
 
     public String encipher(String text){
         String message = text.toUpperCase().replaceAll("[^a-zA-Z ]", "");
-        StringBuilder enc_text = new StringBuilder(""); // String en_text = ''
+        StringBuilder enc_text = new StringBuilder("");
         int shift = 13;
         int len = message.length();
         for (int x = 0; x < len; x++){
             char letter = message.charAt(x);
-            }if (letter >= 'A' && letter <= 'Z'){
+            if (letter >= 'A' && letter <= 'Z'){
                 if (letter > 'M') {
                     letter -= shift;
                 } else {
